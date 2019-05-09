f4100c5cadad6586ae69e45bb85ea5c58a3c34dc
Giovanna Ambrosini <giovanna.ambrosini@epfl.ch>
2019-05-09 12:46:45 +0200

    Packages/paien/tophat (#233)
    * Add TopHat package release 2.1.1
    
    * Merge changes from cornalin
    
    * TopHat package release 2.1.2
    
    * tophat conflicts with gcc@6: (#234)
    
    * a patch exists: https://github.com/DaehwanKimLab/tophat/pull/38
    but might have unexpected side effects

--- var/spack/repos/builtin/packages/tophat/package.py
+++ var/spack/repos/builtin/packages/tophat/package.py
@@ -26,20 +26,34 @@ from spack import *
 
 
 class Tophat(AutotoolsPackage):
-    """Spliced read mapper for RNA-Seq."""
+    """TopHat is a fast splice junction read mapper for RNA-Seq.
+
+     It aligns RNA-Seq reads to mammalian-sized genomes using the ultra
+     high-throughput short read aligner Bowtie, and then analyzes the
+     the mapping results to identify splice junctions between exons."""
 
     homepage = "http://ccb.jhu.edu/software/tophat/index.shtml"
     url      = "https://github.com/infphilo/tophat/archive/v2.1.1.tar.gz"
 
+    version('2.1.2', 'db844fd7f53c519e716cd6222e6195b2')
     version('2.1.1', 'ffd18de2f893a95eb7e9d0c5283d241f')
 
     depends_on('autoconf', type='build')
-    depends_on('automake', type='build')
+    # 2.1.1 only builds with automake@1.15.1.  There's a patch here:
+    # https://github.com/spack/spack/pull/8244, which was incorporated
+    # upstream in 2.1.2, which is known to build with 1.16.1 and 1.15.1.
+    depends_on('automake',                        type='build')
+    depends_on('automake@1.15.1', when='@:2.1.1', type='build')
     depends_on('libtool',  type='build')
     depends_on('m4',       type='build')
 
     depends_on('boost@1.47:')
     depends_on('bowtie2', type='run')
+
+    # uses make_pair which changed signature with C++14
+    # a patch exists: https://github.com/DaehwanKimLab/tophat/pull/38
+    # but there might be unexpected side effects
+    conflicts('%gcc@6:')
 
     parallel = False
 
