57046c71430da5a5dbc84e11ca02ce2ea6f42631
DJ <daniel.jana@epfl.ch>
2019-11-05 17:04:19 +0100

    Update to 2019.4 because there's a serious bug in <2019.4 (#248)

--- var/spack/repos/builtin/packages/gromacs/package.py
+++ var/spack/repos/builtin/packages/gromacs/package.py
@@ -25,6 +25,8 @@ class Gromacs(CMakePackage):
     maintainers = ['junghans', 'marvinbernhardt']
 
     version('develop', branch='master')
+    version('2019.4', sha256='ba4366eedfc8a1dbf6bddcef190be8cd75de53691133f305a7f9c296e5ca1867')
+    version('2019.3', sha256='4211a598bf3b7aca2b14ad991448947da9032566f13239b1a05a2d4824357573')
     version('2019.2', sha256='bcbf5cc071926bc67baa5be6fb04f0986a2b107e1573e15fadcb7d7fc4fb9f7e')
     version('2019.1', sha256='b2c37ed2fcd0e64c4efcabdc8ee581143986527192e6e647a197c76d9c4583ec')
     version('2019', sha256='c5b281a5f0b5b4eeb1f4c7d4dc72f96985b566561ca28acc9c7c16f6ee110d0b')
